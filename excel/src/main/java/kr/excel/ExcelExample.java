95cf0e2fbbdc34676d724d21ae96418bffac8922
robert-min <robertmin522@gmail.com>
2023-08-09 11:09:27 +0900

    feat : Develop excel cell type

--- excel/src/main/java/kr/excel/ExcelExample.java
+++ excel/src/main/java/kr/excel/ExcelExample.java
@@ -2,9 +2,13 @@ package kr.excel;
 
 import org.apache.poi.ss.usermodel.*;
 
+import javax.xml.crypto.Data;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ExcelExample {
     public static void main(String[] args) {
@@ -14,7 +18,42 @@ public class ExcelExample {
             Sheet sheet = workbook.getSheetAt(0);
             for (Row row : sheet) {
                 for (Cell cell : row) {
-                    System.out.print(cell.toString() + "\t");
+                    switch (cell.getCellType()) {
+                        case NUMERIC:
+                            if (DateUtil.isCellDateFormatted(cell)) {
+                                Date dataValue = cell.getDateCellValue();
+                                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                                String formattedDate = dateFormat.format(dataValue);
+                                System.out.print(formattedDate + "\t");
+                            } else {
+                                double numericValue = cell.getNumericCellValue();
+                                if (numericValue == Math.floor(numericValue)) {
+                                    int intValue = (int) numericValue;
+                                    System.out.print(intValue + "\t");
+                                } else {
+                                    System.out.print(numericValue + "\t");
+                                }
+                            }
+                            break;
+                        case STRING:
+                            String stringValue = cell.getStringCellValue();
+                            System.out.print(stringValue + "\t");
+                            break;
+                        case BOOLEAN:
+                            boolean booleanValue = cell.getBooleanCellValue();
+                            System.out.print(booleanValue + "\t");
+                            break;
+                        case FORMULA:
+                            String formulaValue = cell.getCellFormula();
+                            System.out.print(formulaValue + "\t");
+                            break;
+//                        case BLANK:
+//                            System.out.print("\t");
+//                            break;
+                        default:
+                            System.out.print("\t");
+                            break;
+                    }
                 }
                 System.out.println();
             }
