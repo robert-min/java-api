198f4a60ff0a4d199b56645a6a1f95e73488a550
robert-min <robertmin522@gmail.com>
2023-08-13 21:34:27 +0900

    feat : Add Get method

---
+++ rest-api/src/main/java/com/example/restapi/controller/RestApiController.java
@@ -0,0 +1,49 @@
+package com.example.restapi.controller;
+
+import com.example.restapi.model.BookQueryParam;
+import org.apache.catalina.valves.rewrite.InternalRewriteMap;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class RestApiController {
+    @GetMapping(path = "/hello")
+    public String hello() {
+        var html = "<html><body><h1>Hello Spring boot</h1></body></html>";
+        return html;
+    }
+
+    @GetMapping(path = "/echo/{message}/age/{age}/is-man/{isMan}")
+    public String echo(
+            @PathVariable(name = "message") String msg,
+            @PathVariable int age,
+            @PathVariable boolean isMan
+    ) {
+        System.out.println("echo message : " + msg);
+        System.out.println(age + " : " + isMan);
+        return msg.toUpperCase();
+    }
+
+    // http://localhost:8080/api/book?category=IT&issuedYear=2023&issued-month=01&issued_day=31
+    @GetMapping(path = "/book")
+    public void queryParam(
+            @RequestParam String category,
+            @RequestParam String issuedYear,
+            @RequestParam(name = "issued-month") String issuedMonth,
+            @RequestParam String issued_day
+    ) {
+        System.out.println(category);
+        System.out.println(issuedYear);
+        System.out.println(issuedMonth);
+        System.out.println(issued_day);
+    }
+
+
+    // http://localhost:8080/api/book2?category=IT&issuedYear=2023&issued-month=01&issued_day=31
+    @GetMapping(path = "/book2")
+    public void queryParamDto(
+            BookQueryParam bookQueryParam
+    ) {
+        System.out.println(bookQueryParam);
+    }
+}
