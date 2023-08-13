681ed2d1c6eef75afec0c0bdbfd89d8ac471fbd5
robert-min <robertmin522@gmail.com>
2023-08-13 21:59:53 +0900

    feat : Add Post method

---
+++ rest-api/src/main/java/com/example/restapi/controller/PostApiController.java
@@ -0,0 +1,27 @@
+package com.example.restapi.controller;
+
+import com.example.restapi.model.BookRequest;
+import com.example.restapi.model.UserRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PostApiController {
+    @PostMapping("/post")
+    public String post(
+            @RequestBody BookRequest bookRequest
+    ){
+        System.out.println(bookRequest);
+        return bookRequest.toString();
+    }
+
+    @PostMapping("/user")
+    public void User(
+            @RequestBody UserRequest userRequest
+            ) {
+        System.out.println(userRequest);
+    }
+}
