4f7c20a8c7d32612ce7848252967243cfc397677
robert-min <robertmin522@gmail.com>
2023-08-14 10:37:59 +0900

    feat : Add objectMapper

--- rest-api/src/test/java/com/example/restapi/RestApiApplicationTests.java
+++ rest-api/src/test/java/com/example/restapi/RestApiApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.restapi;
 
+import com.example.restapi.model.UserRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestApiApplicationTests {
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws JsonProcessingException {
+        var user = new UserRequest();
+        user.setUserName("홍");
+        user.setUserAge(10);
+        user.setEmail("hong@naver");
+        user.setIsKorean(true);
+
+        var json = objectMapper.writeValueAsString(user);   // 직렬화
+        System.out.println(json);
+
+        var dto = objectMapper.readValue(json, UserRequest.class);  // 역직렬화
+        System.out.println(dto);
     }
 
 }
