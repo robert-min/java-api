4923ff829b9efc20579f8fa003a1e1d16fe916ea
robert-min <robertmin522@gmail.com>
2023-08-14 13:23:31 +0900

    feat : Add validation

---
+++ validation/src/main/java/com/example/validation/model/UserRegisterRequest.java
@@ -0,0 +1,39 @@
+package com.example.validation.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class UserRegisterRequest {
+    @NotBlank
+    private String name;
+
+    @Size(min=1, max=12)
+    @NotBlank
+    private String password;
+
+    @NotNull
+    @Min(1)
+    @Max(100)
+    private Integer age;
+
+    @Email
+    private String email;
+
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "error phone number")
+    private String phoneNumber;
+
+    @FutureOrPresent
+    private LocalDateTime registerAt;
+}
